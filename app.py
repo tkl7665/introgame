b04d5bda48eb2f8aeb7ed82c100f1a8786165152
tkl <tkl7665@gmail.com>
2022-12-01 22:21:45 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,88 @@
+import sys
+import random
+import time
+
+unit_list=['Unit A','Unit B','Unit C','Unit D','Unit E']
+captured_units=[]
+
+def print_list(units):
+	id=0
+	for unit in units:
+		id+=1
+		print('{0}. {1}'.format(id,unit))
+	print()
+
+def option_1():
+	print('List of units: ')
+	print_list(unit_list)
+
+	try:
+		choice=int(input('Your choice: '))-1
+		print()
+
+		if choice>=0 and choice<len(unit_list):
+			print('{0} selected'.format(unit_list[choice]))
+			print('Rolling dice')
+
+			for i in range(5):
+				print('.')
+				time.sleep(1)
+
+			dice_roll=random.randint(1,6)
+			print('{0} rolled!'.format(dice_roll))
+
+			if dice_roll%2==0:
+				print('Success!')
+				captured_units.append(unit_list[choice])
+			else:
+				print('Failed!')
+		else:
+			raise ValueError	
+
+	except ValueError:
+		print('Invalid choice. Integers only')
+		print()
+
+def option_2():
+	count=len(captured_units)
+	if count>0:
+		print('Number Units recruited [{0}]'.format(count))
+		print_list(captured_units)
+
+		print('TO DO: how can you select a unit and have an action with it')
+	else:
+		print('None Captured!')
+
+def start_game():
+	quit=False
+
+	while not quit:
+		print('1. Capture a unit')
+		print('2. List captured unit(s)')
+		print('9. Quit')
+		print()
+
+		try:
+			choice=int(input('Your choice: '))
+			print()
+
+			if choice==1:
+				option_1()
+			elif choice==2:
+				option_2()
+			elif choice==9:
+				print('Bye')
+				quit=True
+			else:
+				print('Option not availale.')
+			print()
+
+		except ValueError:
+			print('Invalid choice. Integers only.')
+			print()
+
+print('Your First Game')
+print('-------------------')
+print('You define what you want to do here')
+
+start_game()
